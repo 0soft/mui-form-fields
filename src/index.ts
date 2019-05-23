c0148f95f9d13f49b6c7864ce711e65a3c53795b
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-05-23 10:06:54 -0300

    Add FormButtonField

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import FormTextField from './components/FormTextField';
 import FormSwitchField from './components/FormSwitchField';
+import FormButtonField from './components/FormButtonField';
 
-export { FormTextField, FormSwitchField };
+export { FormTextField, FormSwitchField, FormButtonField };
