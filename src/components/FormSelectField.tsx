070c0dcc45df1d958db74dc5f2724171d61912d0
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-06-17 15:53:53 -0300

    Add FormSelectField component

---
+++ src/components/FormSelectField.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { FieldInputProps, FieldMetaState } from 'react-final-form';
+import { FieldValidator } from '../utils';
+import FormField from './FormField';
+
+interface Options {
+  label: any;
+  value: string;
+}
+
+interface FormSelectFieldProps {
+  icon?: string | React.ReactElement;
+  hasIcon?: boolean;
+  name: string;
+  label: string;
+  placeholder?: string;
+  disabled?: boolean;
+  options: Options[];
+  validate?: FieldValidator | string;
+  defaultValue?: string;
+}
+
+const FormSelectField: React.FunctionComponent<FormSelectFieldProps> = ({
+  icon = '',
+  hasIcon = true,
+  name,
+  label,
+  placeholder,
+  disabled = false,
+  options,
+  validate,
+  defaultValue,
+}) => {
+  return (
+    <FormField
+      icon={icon}
+      name={name}
+      hasIcon={hasIcon}
+      validate={validate}
+      render={({
+        input,
+        meta,
+      }: {
+        input: FieldInputProps<HTMLElement>;
+        meta: FieldMetaState;
+      }) => {
+        return (
+          <FormControl style={{ width: '100%' }}>
+            <InputLabel shrink>{label}</InputLabel>
+            <Select
+              value={input.value || defaultValue}
+              onChange={input.onChange}
+              input={<Input name={name} />}
+            >
+              {options.map((o: Options) => {
+                return (
+                  <MenuItem key={o.value} value={o.value}>
+                    {o.label}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            <FormHelperText>{meta.touched ? meta.error : ''}</FormHelperText>
+          </FormControl>
+        );
+      }}
+    />
+  );
+};
+
+export default FormSelectField;
