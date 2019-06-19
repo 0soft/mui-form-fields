cafebd1623adce17cb9971c11377981a51c96c24
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-06-19 17:41:49 -0300

    Add basic documentation page

---
+++ doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  codeSandbox: false,
+  themeConfig: {
+    showPlaygroundEditor: true,
+  },
+  htmlContext: {
+    head: {
+      links: [{
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      }]
+    }
+  },
+}
