9099e0d39035fc71021b2ae5f1d81b05d0c22aef
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-06-26 16:46:17 -0300

    Make README.md point to the right documentation pages

--- bin/getComponentsTable.js
+++ bin/getComponentsTable.js
@@ -1,7 +1,7 @@
 const getComponents = require("../devtools/getComponents");
 const components = getComponents();
 const getURL = (component) => {
-  return `https://zerosoft.dev/mui-form-fields/${component}`;
+  return `https://mui-form-fields.0soft.dev/components/${component}`;
 }
 const list = components.map(c => {
   return `- [\`<${c}/>\`](${getURL(c)})`;
