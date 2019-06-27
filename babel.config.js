6eee7f7bd9213a5198a3951f09788173e1d9d0dd
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-06-27 00:05:56 -0300

    Fix import components on mdx files for read of properties

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
-const plugins = [];
-
-if (process.env.NODE_ENV !== 'production') {
-  plugins.push('babel-plugin-typescript-to-proptypes');
-}
-
 module.exports = {
   presets: ['@babel/preset-typescript', '@babel/preset-react'],
-  plugins,
 };
