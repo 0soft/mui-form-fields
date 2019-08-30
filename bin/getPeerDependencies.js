91af45a82130778c04852f808b00dfdaeb218de3
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-08-30 15:18:49 -0300

    Display the exact version of all used dependencies when running `npm i`

--- bin/getPeerDependencies.js
+++ bin/getPeerDependencies.js
@@ -1,18 +1,18 @@
-const packageinfo = require("read-pkg-up").sync().package;
+const packageinfo = require('read-pkg-up').sync().package;
 const peerDependencies = packageinfo.peerDependencies;
-const table = require("markdown-table");
+const table = require('markdown-table');
 const baseTable = Object.entries(peerDependencies).map(([k, v]) => {
   return [`[${k}](https://www.npmjs.com/package/${k})`, `\`${v}\``];
 });
-const dependencyTable = [
-  ["Dependency", "Version"],
-].concat(baseTable);
+const dependencyTable = [['Dependency', 'Version']].concat(baseTable);
 
-const list = Object.keys(peerDependencies).join(" ");
-console.log(table(dependencyTable))
-console.log("")
-console.log("Install all of them via:");
-console.log("")
-console.log("```bash");
+const list = Object.entries(peerDependencies)
+  .map(([d, v]) => `${d}@${v}`)
+  .join(' ');
+console.log(table(dependencyTable));
+console.log('');
+console.log('Install all of them via:');
+console.log('');
+console.log('```bash');
 console.log(`$ npm i --save ${list}`);
-console.log("```");
+console.log('```');
